df88e323ad7e1ddacb714c262b3be88a47e0aa9a
chan9yu <wry5533@gmail.com>
2023-07-17 00:48:28 +0900

    feat: todoStorage 리팩토링

--- src/App.vue
+++ src/App.vue
@@ -10,30 +10,31 @@ import { ref } from 'vue';
 
 import type { Todo } from './@types';
 import { TodoFooter, TodoHeader, TodoInput, TodoList } from './components';
-import { todoStorage } from './services';
+import { getTodos, saveTodos } from './utils';
 
 const todoList = ref<Todo[]>([]);
 
 const fetchTodoList = () => {
-	todoList.value = todoStorage.getTodos().sort((a, b) => a.title.localeCompare(b.title));
+	todoList.value = getTodos().sort((a, b) => a.title.localeCompare(b.title));
 };
 
 const handleAddTodoItem = (item: string) => {
 	todoList.value.push({ done: false, title: item });
-	todoStorage.saveTodos(todoList.value);
+	saveTodos(todoList.value);
 };
 
 const handleToggleTodoItem = (todoItem: Todo, index: number) => {
 	todoList.value.splice(index, 1, { ...todoItem, done: !todoItem.done });
-	todoStorage.saveTodos(todoList.value);
+	saveTodos(todoList.value);
 };
 
 const handleDeleteTodoItem = (index: number) => {
 	todoList.value.splice(index, 1);
-	todoStorage.saveTodos(todoList.value);
+	saveTodos(todoList.value);
 };
 
 fetchTodoList();
 </script>
 
 <style lang="scss" scoped></style>
+./utils
