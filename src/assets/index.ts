32615b739838f0c19df6b21c4edb0dcaf4840be3
chan9yu <wry5533@gmail.com>
2023-07-18 00:04:08 +0900

    feat: assets 파일 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+export { default as stickyNotesImg } from './images/sticky-notes.png';
+
+export { default as addIcon } from './svgs/add.svg';
+export { default as articleIcon } from './svgs/article.svg';
+export { default as checkIcon } from './svgs/check.svg';
+export { default as deleteIcon } from './svgs/delete.svg';
+export { default as editIcon } from './svgs/edit.svg';
