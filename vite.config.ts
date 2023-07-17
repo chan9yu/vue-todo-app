7f8cafc0521302344176352bf1999ba70b0169a1
chan9yu <wry5533@gmail.com>
2023-07-18 00:04:38 +0900

    feat: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import vue from '@vitejs/plugin-vue';
 import autoprefixer from 'autoprefixer';
 import { UserConfig, defineConfig } from 'vite';
+import path from 'path';
+
+const pathSrc = path.resolve(__dirname, './src');
 
 const config = <UserConfig>defineConfig({
 	plugins: [vue()],
@@ -12,7 +15,7 @@ const config = <UserConfig>defineConfig({
 	css: {
 		preprocessorOptions: {
 			scss: {
-				additionalData: `@use "./src/styles/global.scss" as *;`
+				additionalData: `@use "${pathSrc}/styles/global.scss" as *;`
 			}
 		},
 		postcss: {
