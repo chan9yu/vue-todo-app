88401ed6df192c20f3bd532f75862cdf621b4246
chan9yu <wry5533@gmail.com>
2023-07-18 00:03:47 +0900

    feat: .d.ts 파일 정리

---
+++ src/@types/declarations/global.d.ts
@@ -0,0 +1,60 @@
+declare module '*.png' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.jpg' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.jpeg' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.gif' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.woff' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.woff2' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.eot' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.ttf' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.otf' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.svg' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.md' {
+	const content: string;
+	export default content;
+}
+declare module 'shaka-player' {
+	export = shaka;
+}
+declare module 'shaka-player/dist/shaka-player.compiled' {
+	export = shaka;
+}
