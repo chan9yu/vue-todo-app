e236d18b7edcd74ff60ecfbfe750dd601d44a4fe
chan9yu <wry5533@gmail.com>
2023-07-23 20:11:51 +0900

    feat: todo list가 비어있을 때 화면 추가

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,18 +1,26 @@
 <template>
 	<main class="todo-list">
 		<TodoInfo />
-		<ul class="todo-list__wrapper">
+		<ul v-if="todoList.length" class="todo-list__wrapper">
 			<TodoItem v-for="todoItem in todoList" :key="todoItem.id" :todoItem="todoItem" />
 		</ul>
+		<div v-else class="todo-list__empty">
+			<img class="todo-list__empty__img" :src="paperPng" alt="todo item empty" />
+			<div class="todo-list__empty__contents">
+				<span>내용이 비어있습니다.</span>
+				<span>자유롭게 내용을 추가해보세요!</span>
+			</div>
+		</div>
 	</main>
 </template>
 
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
 
+import { paperPng } from '../assets';
+import { TodoInfo, TodoItem } from '../components';
 import { useTodoStore } from '../store';
 import { getTodosFromStorage } from '../utils';
-import { TodoInfo, TodoItem } from '.';
 
 const todoStore = useTodoStore();
 const { todoList } = storeToRefs(todoStore);
@@ -38,6 +46,38 @@ fetchTodoList();
 		display: flex;
 		flex-direction: column;
 		gap: 12px;
+	}
+
+	&__empty {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 16px;
+		padding: 64px 0;
+
+		&__img {
+			width: 48px;
+			height: 48px;
+		}
+
+		&__contents {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			gap: 4px;
+
+			> span {
+				color: $gray-300;
+			}
+
+			:first-of-type {
+				font-weight: 800;
+			}
+
+			:last-of-type {
+				font-weight: 400;
+			}
+		}
 	}
 }
 </style>
